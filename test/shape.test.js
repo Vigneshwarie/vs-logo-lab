8bcc35dea76ad8f94a6876e14927f988e0880a6d
Vigneswari Sambandam <vigneshwarie@gmail.com>
2023-11-30 15:01:14 -0500

    Added functionality for testing square

--- test/shape.test.js
+++ test/shape.test.js
@@ -1,6 +1,7 @@
 const { Rectangle, Square, Circle, Triangle } = require('../lib/shape');
 
 // Created one function to test the functionality of one shape
+// This blocks test the rectangle functionality
 describe('Rectangle', () => {
   describe('render', () => {
     it('should return rectangle', () => {
@@ -9,9 +10,21 @@ describe('Rectangle', () => {
                     <text x="100" y="75" dominant-baseline="middle" text-anchor="middle" font-size="40" fill="White">SVG</text>   
                     </svg>`;
          const createRectange = new Rectangle("150", "200", "Blue", "White", "SVG");
-         console.log(123);
-         console.log(createRectange.render());
           expect(createRectange.render()).toEqual(myRectangle);
+    });
+  });
+});
+
+// This blocks test the square functionality
+describe('Square', () => {
+  describe('render', () => {
+    it('should return square', () => {
+          const mySquare = `<svg width="200" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="0" y="0" width="200" height="200" stroke="black" fill="Green" stroke-width="4"/>
+                    <text x="100" y="100" dominant-baseline="middle" text-anchor="middle" font-size="40" fill="White">SVG</text>   
+                    </svg>`;
+         const createSquare = new Square("200", "Green", "White", "SVG");
+          expect(createSquare.render()).toEqual(mySquare);
     });
   });
 });
